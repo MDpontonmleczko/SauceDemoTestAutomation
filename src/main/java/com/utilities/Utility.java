0e85e7d7fd8e32c74551b2a19231a4561a481f65
Milosz Dudzik <miloszdudzik@gmail.com>
2024-10-17 15:24:27 +0200

    Added created classes for JavaScript execution to utility class

--- src/main/java/com/utilities/Utility.java
+++ src/main/java/com/utilities/Utility.java
@@ -2,9 +2,12 @@ package com.utilities;
 
 import com.utilities.drivers.ChromeDriverManager;
 import com.utilities.drivers.EdgeDriverManager;
+import com.utilities.js.ElementClicker;
+import com.utilities.js.ElementScroller;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class Utility {
+public abstract class Utility {
 
     public static WebDriver getChromeDriver(){
         return ChromeDriverManager.getWebDriver();
@@ -12,5 +15,13 @@ public class Utility {
 
     public static WebDriver getEdgeDriver() {
         return EdgeDriverManager.getWebDriver();
+    }
+
+    public static void scrollToElement(By locator) {
+        ElementScroller.scrollToElementJS(locator);
+    }
+
+    public static void clickElement(By locator) {
+        ElementClicker.clickElementJS(locator);
     }
 }
