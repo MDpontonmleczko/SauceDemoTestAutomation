401084c97109fed34b8bc772b67192565896557e
Milosz Dudzik <miloszdudzik@gmail.com>
2024-10-28 15:46:24 +0100

    Created LoginPage with methods for logging  in (openLoginPageURL, setUsername, setPassword, clickLoginButton) and getting errorMessage.

---
+++ src/main/java/com/saucedemo/LoginPage.java
@@ -0,0 +1,40 @@
+package com.saucedemo;
+
+import com.base.BasePage;
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+    private final By usernameFieldLocator = By.xpath("//*[@id=\"user-name\"]");
+    private final By passwordFieldLocator = By.cssSelector("#password");
+    private final By loginButtonLocator = By.cssSelector("#login-button");
+    private final By loginErrorMessageLocator = By.cssSelector(".login-box h3");
+    private String loginPageURL = "https://www.saucedemo.com/";
+
+    public ProductsPage logIn(String username, String password){
+        openLoginPageURL();
+        setUsername(username);
+        setPassword(password);
+        return clickLoginButton();
+    }
+
+    private void openLoginPageURL() {
+        super.openURL(loginPageURL);
+    }
+
+    private void setUsername(String username) {
+        super.sendText(usernameFieldLocator, username);
+    }
+
+    private void setPassword(String password) {
+        super.sendText(passwordFieldLocator, password);
+    }
+
+    private ProductsPage clickLoginButton() {
+        super.click(loginButtonLocator);
+        return new ProductsPage();
+    }
+
+    public String getErrorMessage() {
+        return super.readText(loginErrorMessageLocator);
+    }
+}
